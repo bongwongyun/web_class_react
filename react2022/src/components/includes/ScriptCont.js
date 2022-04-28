e28c1ab1b235eb41a4927bdfd3b81828b2e0c102
bongwongyun <basaradog355@gmail.com>
2022-04-28 14:14:13 +0900

    2022.04.28

--- react2022/src/components/includes/ScriptCont.js
+++ react2022/src/components/includes/ScriptCont.js
@@ -6,8 +6,8 @@ function ScriptCont() {
       <div className="container">
         <div className="script__inner">
           <img src="/img/script01.jpg" alt="" />
-          <div className="scr__box">
-            <h2>animationRequestFrame</h2>
+          <h2>animationRequestFrame</h2>
+          <div className="scr__text">
             <p>레이아웃은 사이트를 만드는데 가장 기본적이 요소입니다. 레이아웃만 제대로 만들 수 있다면 사이트의 제작에 50%는 끝나다고 보셔도 됩니다. 그만큼 레이아웃 제작은 상당히 중요합니다. 사이트의 제작에 50%는 끝나다고 보셔도 됩니다. 그만큼 레이아웃 제작은 상당히 중요합니다.</p>
             <p>레이아웃은 사이트를 만드는데 가장 기본적이 요소입니다. 레이아웃만 제대로 만들 수 있다면 사이트의 제작에 50%는 끝나다고 보셔도 됩니다. 그만큼 레이아웃 제작은 상당히 중요합니다. 사이트의 제작에 50%는 끝나다고 보셔도 됩니다. 그만큼 레이아웃 제작은 상당히 중요합니다.</p>
           </div>
