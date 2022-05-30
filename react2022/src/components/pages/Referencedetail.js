fd2452ea9685f05423baa776504a911e0df5bddf
bongwongyun <basaradog355@gmail.com>
2022-05-30 11:21:53 +0900

    2022.05.30

--- react2022/src/components/pages/Referencedetail.js
+++ react2022/src/components/pages/Referencedetail.js
@@ -51,50 +51,50 @@ class Referencedetail extends React.Component {
         } else {
             return (
                 <>
-                    <Header />
-                    <ConContacts>
-                    <section className="refer__cont">
-                            <div className="container">
-                                <div className="refer__inner">
-                                    <div className="refer__table">
-                                        <h2>{location.state.title}</h2>
-                                        <p>{location.state.desc}</p>
-                                            <table className='table'>
-                                                <thead>
-                                                    <tr>
-                                                        <th>특징</th>
-                                                        <th>설명</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <th>요소</th>
-                                                        <td>{location.state.element}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th>닫는 태그</th>
-                                                        <td>{location.state.tag}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th>버전</th>
-                                                        <td>{location.state.version}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th>시각적 표현</th>
-                                                        <td>{location.state.view}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th>사용성</th>
-                                                        <td>{location.state.use}</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                    </div>
+                <Header />
+                <ConContacts>
+                <section className="refer__cont">
+                        <div className="container">
+                            <div className="refer__inner">
+                                <div className="refer__table">
+                                    <h2>{location.state.title}</h2>
+                                    <p>{location.state.desc}</p>
+                                        <table className='table'>
+                                            <thead>
+                                                <tr>
+                                                    <th>특징</th>
+                                                    <th>설명</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <th>요소</th>
+                                                    <td>{location.state.element}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>닫는 태그</th>
+                                                    <td>{location.state.tag}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>버전</th>
+                                                    <td>{location.state.version}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>시각적 표현</th>
+                                                    <td>{location.state.view}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>사용성</th>
+                                                    <td>{location.state.use}</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
                                 </div>
                             </div>
-                        </section>
-                    </ConContacts>
-                    <Footer />
+                        </div>
+                    </section>
+                </ConContacts>
+                <Footer />
                 </>
             )
         }
